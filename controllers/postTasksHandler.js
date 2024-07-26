f668c3f1b88b7801db335c6523c9641bde219a27
Alihan <alianonalbekov@gmail.com>
2024-07-26 10:49:30 +0500

    Fix bug

--- controllers/postTasksHandler.js
+++ controllers/postTasksHandler.js
@@ -3,9 +3,18 @@ const fs = require('fs');
 let tasks = [];
 
 function postTasksHandler(req, res) {
-    tasks.push({ id: tasks.length + 1, ...req.body });
-    fs.writeFile('./data/data.txt', JSON.stringify(tasks), 'utf-8', (err) => {
+    fs.readFile('./data/data.txt', (err, data) => {
         if (err) console.log(err);
+        tasks = JSON.parse(data);
+        tasks.push({ id: tasks.length + 1, ...req.body });
+        fs.writeFile(
+            './data/data.txt',
+            JSON.stringify(tasks),
+            'utf-8',
+            (err) => {
+                if (err) console.log(err);
+            }
+        );
     });
     res.send('<h1>Task was added.</h1><a href="/">One more</a>');
 }
