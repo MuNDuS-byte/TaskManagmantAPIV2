856eec74bb95e1731981f75894cefc14adbf8251
Alihan <alianonalbekov@gmail.com>
2024-07-26 13:38:08 +0500

    Fix bug

--- controllers/getTasksHandler.js
+++ controllers/getTasksHandler.js
@@ -3,8 +3,13 @@ const fs = require('fs');
 function getTasksHandler(req, res) {
     fs.readFile('./data/data.txt', (err, data) => {
         if (err) console.log(err);
-        const body = JSON.parse(data);
-        res.json(body);
+        if (req.headers['content-type'] === 'application/json') {
+            const body = JSON.parse(data);
+            res.json(body);
+        }
+        res.send(
+            `<h1>You don't have any task</h1> <a href="/">Back to the root</a>`
+        );
     });
 }
 
